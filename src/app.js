56437a2c497de76b3daccf7583d4d21ae44aa4dd
Tanmayi G <tanmayigraju@gmail.com>
2025-06-13 09:49:54 +0530

    Implemented CRUD APIs

--- src/app.js
+++ src/app.js
@@ -5,20 +5,66 @@ require("dotenv").config();
 
 const app = express();
 
-app.post("/signup", async (req, res) => {
-  //mockData
-  const user = new User({
-    firstName: "Tanmayi",
-    lastName: "G",
-    emailId: "tanmayiig@gmail.com",
-    password: "tanmayi@2004",
-  });
+app.use(express.json());
 
+app.post("/signup", async (req, res) => {
+  const user = new User(req.body);
   try {
     await user.save();
     res.send("User added successfully");
   } catch (err) {
     res.status(400).send("Error adding the user" + err.message);
+  }
+});
+
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const user = await User.find({ emailId: userEmail });
+    if (!user) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(user);
+    }
+  } catch {
+    res.send("Something went wrong");
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    if (!users) {
+      res.status(404).send("No users found");
+    } else {
+      res.send(users);
+    }
+  } catch {
+    res.send("Something went wrong");
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    res.send("User deleted successfully");
+  } catch {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+    const user = await User.findByIdAndUpdate(userId, data, {
+      returnDocument: "after",
+    });
+    console.log(user);
+    res.send("User updated successfully");
+  } catch {
+    res.send("Something went wrong");
   }
 });
 
