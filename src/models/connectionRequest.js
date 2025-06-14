50142727ff92935f3534ebee6741ed64fce6b1f9
Tanmayi G <tanmayigraju@gmail.com>
2025-06-15 00:39:01 +0530

    Created connectionRequest schema and API, implemented mongoose pre-save hook and compound indexing

---
+++ src/models/connectionRequest.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const connectionRequestSchema = new mongoose.Schema(
+  {
+    fromUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+    toUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: {
+        values: ["ignored", "interested", "accepted", "rejected"],
+        message: `Incorrect status type: {VALUE}`,
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+//Mongoose compound indexing for query optimization
+connectionRequestSchema.index({ fromUserId: 1, toUserId: 1 });
+
+//Mongoose pre-save hook
+connectionRequestSchema.pre("save", function (next) {
+  const connectionRequest = this;
+  if (connectionRequest.fromUserId.equals(connectionRequest.toUserId)) {
+    throw new Error("Cannot send connection request to yourself!");
+  }
+  next();
+});
+
+const ConnectionRequest = mongoose.model("ConnectionRequest", connectionRequestSchema);
+
+module.exports = ConnectionRequest;
