2b63c5e4b68b1c96a32ac322fb4204b36ba39aff
Tanmayi G <tanmayigraju@gmail.com>
2025-06-16 13:46:34 +0530

    Added CORS middleware

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const connectDB = require("./config/database");
 require("dotenv").config();
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 const authRouter = require("./routers/auth");
 const profileRouter = require("./routers/profile");
@@ -10,6 +11,12 @@ const userRouter = require("./routers/user");
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
